edae582229c354c56ee14ff37856fd2c6e23c2b5
Hwngmai <hungmai0203@gmail.com>
2022-06-16 17:23:59 +0700

    1st commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// numberGenerator
+jQuery(document).ready(function( $ ) {
+    $('.counter').counterUp({
+        delay: 20,
+        time: 1000 
+    });
+    });
+// owlCarousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+    }
+})
+// SmoothScroll
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
